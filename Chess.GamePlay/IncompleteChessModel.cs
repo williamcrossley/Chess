26550795d263169b7397e4be61572829eeb6f111
williamcrossley <crossley.will62@gmail.com>
2023-06-30 13:31:36 +1000

    Untested check blocking save
    Added check blocking, and checking piece capture (untested).
    Saving before attempting bug fix on disappearing pieces

--- Chess.GamePlay/IncompleteChessModel.cs
+++ Chess.GamePlay/IncompleteChessModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.InteropServices;
 using System.Collections;
+using System.Numerics;
 
 namespace Chess.GamePlay
 {
@@ -239,16 +240,15 @@ namespace Chess.GamePlay
             {
                 int[] kingPos = FindKingPosition(board, player);
                 int checkingPiecesCount = 0;
-                ArrayList checkingPieces = new ArrayList(); //struct: <piece>, <row>, <col>... (would've been used for blocking/capture of checking piece)
+                ArrayList checkingPieces = new ArrayList(); //struct: <row>, <col>... (would've been used for blocking/capture of checking piece)
 
                 for (int row = 0; row < board.Length; row++)
-                { //find all checking pieces (not needed yet)
+                { //find all checking pieces (not needed yet, could be removed but will be needed when checking for blocks / captures)
                     for (int col = 0; col < board.Length; col++)
                     {
                         if (IsPieceMoveLegal(board, new Move(row, col, kingPos[0], kingPos[1]), player))
                         {
                             checkingPiecesCount++;
-                            checkingPieces.Add(board[row][col]);
                             checkingPieces.Add(row);
                             checkingPieces.Add(col);
                         }
@@ -273,7 +273,26 @@ namespace Chess.GamePlay
                 }
 
                 if (checkingPiecesCount >= 2) return true; //if double checked and the king cant move, game is over
-              
+
+                //get the path, this only runs if the king can't move AND there is 1 piece checking, otherwise the game isnt over, so we know checkingPieces only has 2 elements
+                int[][] checkingPaths = GetPiecePathPositions(board, new Move((int)checkingPieces[0], (int)checkingPieces[1], kingPos[0], kingPos[1]));
+                foreach (int[] pathSquare in checkingPaths)
+                {
+                    for (int row = 0; row < board.Length; row++)
+                    { 
+                        for (int col = 0; col < board.Length; col++)
+                        {
+                            if (IsPieceOwnedByPlayer(board[row][col], player) &&  IsPieceMoveLegal(board, new Move(row, col, pathSquare[0], pathSquare[1]), player)) 
+                            {
+                                //if the piece at row,col is the players and can move to the current path square, it can block the check or take the checking piece.
+                                return false;
+                            }
+                        }
+                    }
+                }
+                
+                
+
                 return true;
                 
             }
@@ -295,6 +314,67 @@ namespace Chess.GamePlay
                 }
             }
             return new int[] { -1, -1 };
+        }
+
+        private int[][] GetPiecePathPositions(char[][] board, Move move)
+        {   //get path positions INCLUDING start position to be able to check for capture
+            int rowDifference = move.toRow - move.fromRow;
+            int columnDifference = move.toColumn - move.fromColumn;
+
+            if (board[move.fromRow][move.fromColumn].ToString().ToLower() == "r") //if rook
+            {
+                if(rowDifference == 0)
+                {
+                    return GetMovePathPositions(board, move.fromRow, move.fromColumn, Math.Abs(columnDifference), 0, Math.Clamp(columnDifference, -1, 1));
+                }
+
+                if (columnDifference == 0)
+                {
+                    return GetMovePathPositions(board, move.fromRow, move.fromColumn, Math.Abs(rowDifference), Math.Clamp(rowDifference, -1, 1), 0);
+                }
+            }
+
+            if(board[move.fromRow][move.fromColumn].ToString().ToLower() == "b")
+            {
+                return GetMovePathPositions(board, move.fromRow, move.fromColumn, Math.Abs(rowDifference), Math.Clamp(rowDifference, -1, 1), Math.Clamp(columnDifference, -1, 1));
+            }
+
+            if (board[move.fromRow][move.fromColumn].ToString().ToLower() == "q")
+            {
+                if (rowDifference == 0)
+                {
+                    return GetMovePathPositions(board, move.fromRow, move.fromColumn, Math.Abs(columnDifference), 0, Math.Clamp(columnDifference, -1, 1));
+                }
+
+                if (columnDifference == 0)
+                {
+                    return GetMovePathPositions(board, move.fromRow, move.fromColumn, Math.Abs(rowDifference), Math.Clamp(rowDifference, -1, 1), 0);
+                }
+
+                else
+                {
+                    return GetMovePathPositions(board, move.fromRow, move.fromColumn, Math.Abs(rowDifference), Math.Clamp(rowDifference, -1, 1), Math.Clamp(columnDifference, -1, 1));
+                }
+            }
+
+            if (board[move.fromRow][move.fromColumn].ToString().ToLower() == "n" || board[move.fromRow][move.fromColumn].ToString().ToLower() == "p") //just return position as there is no blocking but piece could be captued.
+            {
+                int[][] piecePos = new int[1][];
+                piecePos[0] = new int[] { move.fromRow, move.fromColumn };
+                return piecePos;
+            }
+
+            return null;
+        }
+
+        private int[][] GetMovePathPositions(char[][] board, int startRow, int startColumn, int moveLength, int incrementRow, int incrementColumn)
+        { //adds each space covered by a move to a list and returns that list.
+            int[][] positions = new int[moveLength][];
+            for (int i = 0; i < moveLength; i++)
+            {
+                positions[i] = new int[] { startRow + i * incrementRow, startColumn + i * incrementColumn };
+            }
+            return positions;
         }
     }
 }
